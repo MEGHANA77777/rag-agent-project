45ae5431902efb9791bf753fa2ab3df7cb142831
Y Meghana <23wh1a1276bvrithyderabad.edu.in>
2025-12-30 07:17:26 +0530

    feat: Changed the google studioapi key to the openai api key. Need to find the api key that is free and useful for long time.

--- vector_db.py
+++ vector_db.py
@@ -1,25 +1,26 @@
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance, PointStruct
 
+
 class QdrantStorage:
     def __init__(self, url="http://localhost:6333", collection="docs", dim=3072):
         self.client = QdrantClient(url=url, timeout=30)
         self.collection = collection
-        if not self.client.collection(self.collection):
+        if not self.client.collection_exists(self.collection):
             self.client.create_collection(
                 collection_name=self.collection,
                 vectors_config=VectorParams(size=dim, distance=Distance.COSINE),
             )
-    
+
     def upsert(self, ids, vectors, payloads):
         points = [PointStruct(id=ids[i], vector=vectors[i], payload=payloads[i]) for i in range(len(ids))]
-        self.client.upsert(self.collection points=points)
+        self.client.upsert(self.collection, points=points)
 
-    def search(self, quer_vector, top_k: int = 5):
+    def search(self, query_vector, top_k: int = 5):
         results = self.client.search(
             collection_name=self.collection,
             query_vector=query_vector,
-            with_payloads=True,
+            with_payload=True,
             limit=top_k
         )
         contexts = []
@@ -32,5 +33,5 @@ class QdrantStorage:
             if text:
                 contexts.append(text)
                 sources.add(source)
-        
+
         return {"contexts": contexts, "sources": list(sources)}
